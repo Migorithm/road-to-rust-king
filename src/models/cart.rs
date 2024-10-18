001748c209e2f8a71b39d5fa458062ad6abd102b
Frenchkebab <tubowangin@gmail.com>
2024-10-18 10:35:35 +0900

    add logic for exceptional case

--- src/models/cart.rs
+++ src/models/cart.rs
@@ -15,23 +15,46 @@ pub struct Cart {
     pub(crate) lines: Vec<CartLine>,
 }
 
+//? total의 처리를 Controller에서 해야할지 Model에서 해야할지 잘 모르겠음...
+
 impl Cart {
-    pub fn add_line(&mut self, command: AddCartLine) {
+    pub fn add_line(&mut self, command: AddCartLine) -> Result<(), String> {
+        // 만약 카트 라인 중에 이미 같은 product_id를 가진 라인이 존재하면 에러 반환
+        if self
+            .lines
+            .iter()
+            .any(|line| line.product_id == command.product_id)
+        {
+            return Err("Product Already exists".to_string());
+        }
+
+        // 새로운 CartLine을 생성하여 라인에 추가
         let line = CartLine {
             product_id: command.product_id,
             quantity: command.quantity,
         };
 
         self.lines.push(line);
+
+        Ok(())
     }
 
-    pub fn update_line(&mut self, command: UpdateCartLine) {
-        let line = CartLine {
-            product_id: command.product_id,
-            quantity: command.quantity,
-        };
+    pub fn update_line(&mut self, command: UpdateCartLine) -> Result<(), String> {
+        // 만약 command의 quantity가 0이면, 해당 product_id를 가진 라인을 제거
+        if command.quantity == 0 {
+            self.lines
+                .retain(|line| line.product_id != command.product_id);
+        } else {
+            // quantity > 0인 경우 새로운 CartLine을 생성하여 라인에 추가
+            let line = CartLine {
+                product_id: command.product_id,
+                quantity: command.quantity,
+            };
 
-        self.lines.push(line);
+            self.lines.push(line);
+        }
+
+        Ok(())
     }
 }
 
