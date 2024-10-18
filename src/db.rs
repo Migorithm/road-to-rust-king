05194bb73c9d4e9f3cc7fbcd660e983cda7f7348
Frenchkebab <tubowangin@gmail.com>
2024-10-18 10:25:46 +0900

    fix db structure (vector to hashmap)

--- src/db.rs
+++ src/db.rs
@@ -1,88 +1,78 @@
+use lazy_static::lazy_static;
+
 use std::{
+    collections::HashMap,
     fmt::Error,
-    sync::{LazyLock, RwLock},
+    sync::{LazyLock, Mutex, RwLock},
 };
 
 use crate::models::cart::{Cart, CartLine};
 
-pub static FAKE_CART_DB: LazyLock<RwLock<CartDb>> = LazyLock::new(Default::default);
-
-#[derive(Default)]
-pub struct CartDb {
-    carts: Vec<Cart>,
+// 가라로 DB를 위한 전역 변수
+lazy_static! {
+    static ref CART_DB: Mutex<HashMap<u32, Cart>> = Mutex::new(HashMap::new());
 }
 
-impl CartDb {
-    /// 유저의 장바구니를 찾는다
-    pub fn find_by_user_id(&mut self, user_id: u32) -> &mut Cart {
-        self.carts
-            .iter_mut()
-            .find(|c| c.user_id == user_id)
-            .unwrap()
-    }
+pub fn find_cart_by_user_id(user_id: u32) -> Option<Cart> {
+    // CART_DB의 Mutex를 잠금
+    let db = CART_DB.lock().unwrap();
 
-    // TODO: DB Error 구현하기
-    // TODO: Application 레벨에서 Error 구현하기
-    // TODO: DB단에 Business Logic 넣지 말기 (진짜 DB Operation에서 예상되는 것만!! -- Model이랑 분리!!)
+    // user_id로 Cart를 검색하고, 없으면 None을 반환
+    db.get(&user_id).cloned()
+}
 
-    pub fn create(&mut self, user_id: u32) -> Result<(), Error> {
-        let cart = Cart {
-            user_id,
+pub fn save_cart(cart: Cart) -> Result<(), Error> {
+    // CART_DB의 Mutex를 잠금
+    let mut db = CART_DB.lock().unwrap();
+
+    // user_id를 키로 하여 Cart를 삽입
+    db.insert(cart.user_id, cart);
+
+    Ok(())
+}
+
+// test: 같은 상품에 대한 cartline이 추가되었을 때, 수량만 올라가야 한다
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::models::command::AddCartLine;
+
+    #[test]
+    fn test_add_same_product_increases_quantity() {
+        let mut cart = Cart {
+            user_id: 1,
             total: 0,
             lines: vec![],
         };
 
-        self.carts.push(cart); //? erorr handling?
+        let command1 = AddCartLine {
+            user_id: 1,
+            product_id: 101,
+            quantity: 2,
+        };
 
-        Ok(())
-    }
+        let command2 = AddCartLine {
+            user_id: 1,
+            product_id: 101,
+            quantity: 3,
+        };
 
-    pub fn insert_line(&mut self, cart: &mut Cart, line: CartLine) -> Result<(), Error> {
-        let line_index = cart.lines.iter_mut().enumerate().find_map(|(index, l)| {
-            if l.product_id == line.product_id {
-                Some(index)
-            } else {
-                None
-            }
-        });
+        cart.add_line(command1);
+        cart.add_line(command2);
 
-        if let Some(index) = line_index {
-            cart.lines[index].quantity += line.quantity;
-            Ok(())
-        } else {
-            cart.lines.push(line);
-            Ok(())
-        }
-    }
-
-    pub fn update_line(
-        &mut self,
-        cart: &mut Cart,
-        product_id: u32,
-        quantity: u32,
-    ) -> Result<(), Error> {
-        //? 이렇게 하는게 맞나?
-        let line_index = cart.lines.iter_mut().enumerate().find_map(|(index, l)| {
-            if l.product_id == product_id {
-                Some(index)
-            } else {
-                None
-            }
-        });
-
-        if let Some(index) = line_index {
-            let line = &mut cart.lines[index];
-            if quantity > 0 {
-                line.quantity = quantity;
-            } else {
-                cart.lines.remove(index);
-            }
-        }
-
-        Ok(())
+        assert_eq!(cart.lines.len(), 1);
+        assert_eq!(cart.lines[0].quantity, 5);
     }
 }
 
-// test: 같은 상품에 대한 cartline이 추가되었을 때, 수량만 올라가야 한다
 // advanced: order가 만들어지면, cart를 비운다
+pub fn clear_cart(user_id: u32) -> Result<(), Error> {
+    let mut db = CART_DB.lock().unwrap();
+
+    if let Some(cart) = db.get_mut(&user_id) {
+        cart.lines.clear();
+    }
+
+    Ok(())
+}
 // -> 근데 만약에 pay를 하고 cancel이 되면???
