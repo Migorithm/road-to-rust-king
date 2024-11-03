e66429b7aa3769a9f0a18a4872d33862f93292d2
Migo <saka90030@gmail.com>
2024-11-03 09:53:55 +0400

    rm wrapper type

--- src/db/db.rs
+++ src/db/db.rs
@@ -1,7 +1,7 @@
 use crate::model::cart::Cart;
 
 // smart pointer => Arc, MutexGuard, Rc, Cell, Box, String
-pub struct CartDb(Vec<Option<Cart>>);
+pub struct CartDb(Vec<Cart>);
 
 #[derive(Debug)]
 pub enum CartDbError {
@@ -10,12 +10,12 @@ pub enum CartDbError {
 
 impl CartDb {
     pub fn new() -> Self {
-        Self(Vec::<Option<Cart>>::new())
+        Self(Vec::<Cart>::new())
     }
 }
 
 impl std::ops::Deref for CartDb {
-    type Target = Vec<Option<Cart>>;
+    type Target = Vec<Cart>;
 
     fn deref(&self) -> &Self::Target {
         &self.0
--- src/repository/repository.rs
+++ src/repository/repository.rs
@@ -7,63 +7,36 @@ pub enum RepositoryError {
     TimeOut,
     BackEndDBError,
 }
-#[derive(Debug)]
-pub enum SqlxError {
-    PoolTimedOut,
-    BackEndDBError,
-}
-
-impl From<SqlxError> for RepositoryError {
-    fn from(error: SqlxError) -> Self {
-        match error {
-            SqlxError::PoolTimedOut => RepositoryError::TimeOut,
-            _ => RepositoryError::BackEndDBError,
-        }
-    }
-}
 
 // From Trait? map_err ?
 
 // Error? - network error, duplicate primary key error or other constraints, transaction error
 impl CartRepository {
-    pub fn get_cart(command: &GetCart) -> Result<Cart, RepositoryError> {
+    pub fn get_cart(command: &GetCart) -> Result<Option<Cart>, RepositoryError> {
         let cart = DB
             .lock()
             .unwrap()
             .iter()
-            .find_map(|cart| {
-                cart.as_ref()
-                    .filter(|c| c.user_id == command.user_id)
-                    .cloned()
-            })
-            // TODO
-            .ok_or_else(|| {
-                println!("Cart not found");
-                RepositoryError::BackEndDBError
-            })?;
+            .find(|cart| cart.user_id == command.user_id)
+            .cloned();
 
         Ok(cart)
     }
 
     pub fn create_cart(command: &CreateCart) -> Result<(), RepositoryError> {
-        DB.lock().unwrap().push(Some(command.new_cart.clone()));
+        DB.lock().unwrap().push(command.new_cart.clone());
 
         Ok(())
     }
 
     pub fn update_cart(command: &UpdateCart) -> Result<(), RepositoryError> {
         let mut db = DB.lock().unwrap();
-
-        // let cart_index = db.iter()
-        // .position(|c| c.as_ref().unwrap().user_id == command.cart.user_id)
-        // .unwrap();
-
         let existing_cart = db
             .iter_mut()
-            .find(|c| c.as_ref().unwrap().user_id == command.cart.user_id)
+            .find(|c| c.user_id == command.cart.user_id)
             .unwrap();
 
-        *existing_cart = Some(command.cart.clone());
+        *existing_cart = command.cart.clone();
 
         Ok(())
     }
@@ -73,7 +46,7 @@ impl CartRepository {
 
         let cart_index = db
             .iter()
-            .position(|c| c.as_ref().unwrap().user_id == command.user_id)
+            .position(|c| c.user_id == command.user_id)
             .unwrap();
 
         db.swap_remove(cart_index);
